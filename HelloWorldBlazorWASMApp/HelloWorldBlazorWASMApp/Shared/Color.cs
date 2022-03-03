ad91c5db4d864ccf4f0b24033145d0872b6b2625
Eric Greene <eric@t4d.io>
2022-03-03 18:07:55 -0500

    add course 2 demos

---
+++ HelloWorldBlazorWASMApp/HelloWorldBlazorWASMApp/Shared/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldBlazorWASMApp.Shared
+{
+  public class Color
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Hexcode { get; set; }
+  }
+}
